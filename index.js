1a36fb28d99562c6975ee00c5f3553d9517ec06f
ESMERALDA KOK <52359922+Esmeliefie@users.noreply.github.com>
2022-09-14 09:11:29 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+let myLeads = []
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})  
+function renderLeads(){
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+             `
+    }
+    ulEl.innerHTML = listItems
+}
